cce6f56b414c9d1ad510dca52220be4565abf7b0
tscholl44 <tscholl4444@gmail.com>
2025-06-03 14:29:57 -0400

    Create Chat.js

---
+++ components/Chat.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Chat = ({ route, navigation }) => {
+  const { name, bgColor } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
+
+  return (
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <Text>Welcome to Chat</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default Chat;
