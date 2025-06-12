b2663402a656c32499ecfdee8a10830b4a3dd3dc
tscholl44 <tscholl4444@gmail.com>
2025-06-12 11:54:06 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+// import firestore
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -8,10 +14,28 @@ import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Create the navigator
+
+// create the navigator
 const Stack = createNativeStackNavigator();
 
+
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCcTJAJX9tgg2vKgnZvC1jGW5jZdvBMlfM",
+    authDomain: "chat-app-99ab2.firebaseapp.com",
+    projectId: "chat-app-99ab2",
+    storageBucket: "chat-app-99ab2.firebasestorage.app",
+    messagingSenderId: "428948498459",
+    appId: "1:428948498459:web:82314807d7e73366b8b0be"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -22,9 +46,9 @@ const App = () => {
           component={Start}
         />
         <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+          name="Chat">
+          {props => <Chat db={db} Storage={Storage} isConnected={netInfo.isConnected} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
